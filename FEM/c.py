7c993a46dc4d9a8feadc483ed5fe66cfb4c5fe76
Dmitry V. Sokolov <ha@haqr.eu>
2019-11-23 10:24:16 +0100

    fem typos

--- FEM/c.py
+++ FEM/c.py
@@ -3,35 +3,26 @@ import matplotlib.pyplot as plt
 
 #rhs:radcan(radcan(integrate(n*(x-(i-1)/n)*(x+1),x,(i-1)/n,i/n)) + radcan(integrate(n*((i+1)/n-x)*(x+1),x,i/n,(i+1)/n)));
 
-# right hand side
-def rhs(n, i):
-    if (i==n-1):
-        return -(n+i)/n**2 + n
-    return -(n+i)/n**2
-
 # initialize the data array
-n = 3
-x = [0.] * (n+1)
-x[0] = 0
-x[n] = 1
+n = 5
+phi = [0.]*(n+1)
+phi[0] = 0
+phi[n] = 1
 
-
+# solve the linear system
 for iter in range(1000):
     for i in range(1,n):
-        c = rhs(n,i)/n
-        print(i, c)
-        x[i] = (x[i-1]+x[i+1] - c)/2.
+        rhs = (n+i)/n**3
+        phi[i] = (phi[i-1] + phi[i+1] - rhs)/2.
 
+# plot the solution
+plt.plot(np.linspace(0., 1., n+1), phi, 'gs-', label='FEM solution')
 
-plt.plot([0,1/3, 2/3, 1], x, 'bs-', label='prescribed curvature + constraints')
+# plot the ground truth
+X = np.linspace(0., 1., 100)
+G = [x**3/6 + x**2/2 + x/3 for x in X]
+plt.plot(X, G, 'r-', label='ground truth')
+
 plt.legend()
-
-
-T = np.arange(0,1,.01)
-G = []
-for t in T:
-    G.append(t**3/6 + t**2/2 + t/3)
-
-plt.plot(T, G)
 plt.show()
 
