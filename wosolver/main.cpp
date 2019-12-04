5f4cc1d1e5e47f768da01532821c991ba3850169
Dmitry V. Sokolov <ha@haqr.eu>
2019-12-04 08:03:13 +0100

    new figures

--- wosolver/main.cpp
+++ wosolver/main.cpp
@@ -11,11 +11,11 @@ int main(void) {
     }
 
     // smooth the surface through Gauss-Seidel iterations
-    for (int it=0; it<100; it++) {
+    for (int it=0; it<10000; it++) {
         for (int v=0; v<m.nverts(); v++) {
             if (m.is_boundary_vert(v)) continue; // fix the boundary
 
-            m.point(v) = m.one_ring_barycenter(v) + curv[v]*2.1;
+            m.point(v) = m.one_ring_barycenter(v) + curv[v]*1.5;
         }
     }
 
