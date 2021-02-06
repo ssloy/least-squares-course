a6d2bf9316e07bfe6fc9f3fd301fe5ab020baeda
Dmitry V. Sokolov <ha@haqr.eu>
2021-02-06 11:51:00 +0100

    python port

--- manuscript/listings/poisson-image-editing-1d.py
+++ manuscript/listings/poisson-image-editing-1d.py
@@ -1,20 +1,10 @@
 import numpy as np
-
-fa = 1 # left constraint
-fb = 3 # right constraint
-n = 40
-x = np.linspace(0, 2*np.pi, n)
-g = [np.sin(p) for p in x]
-
+n,f0,fn = 32,1.,3.
+g = [np.sin(x) for x in np.linspace(0, 2*np.pi, n)]
 A = np.matrix(np.zeros((n-1,n-2)))
-b = np.matrix(np.zeros((n-1,1)))
-A[0,0]      =  1
-b[0,0]      =  fa + g[1]-g[0]
-A[n-2, n-3] = -1
-b[n-2,0]    = -fb + g[-1]-g[-2]
-for i in range(1,n-2):
-    b[i,0] = g[i]-g[i-1]
-    A[i, i-1] = -1
-    A[i, i  ] =  1
-
-f = [fa] + (np.linalg.inv(A.T*A)*A.T*b).T.tolist()[0] + [fb]
+np.fill_diagonal(A,      1)
+np.fill_diagonal(A[1:], -1)
+b = np.matrix([[g[i]-g[i-1]] for i in range(1,n)])
+b[ 0,0] = b[ 0,0] + f0
+b[-1,0] = b[-1,0] - fn
+f = [f0] + (np.linalg.inv(A.T*A)*A.T*b).T.tolist()[0] + [fn]
